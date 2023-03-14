31d97026e86614c1b19d2adef39e93ff681f1f3f
Jack Harbon (he/him) <95174793+jackharbon@users.noreply.github.com>
2023-03-14 13:08:13 +0500

    Project frontend

--- walkerrr-frontend/lib/common/single_quest.dart
+++ walkerrr-frontend/lib/common/single_quest.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:walkerrr/auth.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:walkerrr/common/styling_variables.dart';
 import 'package:walkerrr/providers/user_provider.dart';
 import 'package:walkerrr/services/api_connection.dart';
 import "package:walkerrr/providers/step_provider.dart" as globalSteps;
 
+Future<void> signOut() async {
+  await Auth().signOut();
+}
+
 void checkCompletion(progress, currentQuest, reward) {
   if (progress == 1) {
     patchComplete(userObject['uid'], currentQuest);
     patchCoins(userObject['uid'], reward);
+    signOut();
   }
   print('---- currentQuest on single_quest:\n$currentQuest');
 }
@@ -47,7 +53,7 @@ class _SingleQuestState extends State<SingleQuest> {
   bool isQuestFinished = false;
   bool isQuestClaimed = false;
   var currentQuests = userObject["quests"];
-  String buttonText = "Start Quest?";
+  String buttonText = "Start Quest? (*)";
   double progress = 0.0;
   double progressCalc = 0.0;
   var _buttonBackgroundColor = GlobalStyleVariables.questsButtonReady;
@@ -82,7 +88,7 @@ class _SingleQuestState extends State<SingleQuest> {
     }
 
     if (isQuestReady) {
-      buttonText = "Start Quest?";
+      buttonText = "Start Quest? (*)";
       _buttonBackgroundColor = GlobalStyleVariables.questsButtonReady;
       progress = 0.0;
     }
@@ -105,7 +111,7 @@ class _SingleQuestState extends State<SingleQuest> {
       progress = 0.0;
     }
     if (isQuestFinished) {
-      buttonText = 'Claim ${widget.reward} coins';
+      buttonText = 'Claim ${widget.reward} coins (*)';
       _buttonBackgroundColor = GlobalStyleVariables.questsButtonFinished;
       progress = 1.0;
     }
@@ -132,6 +138,7 @@ class _SingleQuestState extends State<SingleQuest> {
       userObject["quests"] = [newQuest];
       currentQuests = userObject["quests"];
       getUserFromDB(userObject['uid']);
+      signOut();
       print(
           '---- startQuest - userObject["quests"] on single_quest:\n${userObject["quests"]}');
     }
@@ -152,70 +159,72 @@ class _SingleQuestState extends State<SingleQuest> {
 
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  widget.questTitle,
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-                Text("Reward: ${widget.reward} coins"),
-              ],
-            ),
-          ),
-          // ! text for test to DELETE
-          Text(
-              'Ready:$isQuestReady Active:$isQuestActive Inactive:$isQuestInactive Finished:$isQuestFinished Claimed:$isQuestClaimed\n'),
-          Text('questTitle:${widget.questTitle}'),
-          Text(
-              'globalSteps:${globalSteps.globalSteps} \nquestCurrent=${widget.questCurrent} questOffset=${widget.questOffset} questGoal=${widget.questGoal}\nprogress: $progress\nprogressCalc: $progressCalc'),
-          Text(
-              'currentQuests:\n$currentQuests\nuserObject["quests"]:\n${userObject["quests"]}'),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: LinearPercentIndicator(
-              width: MediaQuery.of(context).size.width - 80,
-              animation: true,
-              lineHeight: 26.0,
-              backgroundColor: _barBackgroundColor,
-              progressColor: GlobalStyleVariables.questsProgressBar,
-              barRadius: const Radius.circular(20.0),
-              leading: const Icon(Icons.directions_walk_outlined),
-              animationDuration: 2000,
-              percent: progress,
-              center: Text(
-                "Progress: ${(progress * 100).toInt()}%",
-                style: const TextStyle(
-                    color: GlobalStyleVariables.primaryTextLightColour),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    widget.questTitle,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  Text("Reward: ${widget.reward} coins"),
+                ],
               ),
             ),
-          ),
-          if (isQuestClaimed)
-            const Text(
-              'Sign Out to update coins',
-              style: TextStyle(
-                  color: GlobalStyleVariables.questsButtonFinished,
-                  fontSize: 16),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 80,
+                animation: true,
+                lineHeight: 26.0,
+                backgroundColor: _barBackgroundColor,
+                progressColor: GlobalStyleVariables.questsProgressBar,
+                barRadius: const Radius.circular(20.0),
+                leading: const Icon(Icons.directions_walk_outlined),
+                animationDuration: 2000,
+                percent: progress,
+                center: Text(
+                  "Progress: ${(progress * 100).toInt()}%",
+                  style: const TextStyle(
+                      color: GlobalStyleVariables.primaryTextLightColour),
+                ),
+              ),
             ),
-          ElevatedButton(
-              onPressed: isQuestReady
-                  ? startQuest
-                  : isQuestFinished
-                      ? claimQuest
-                      : noAction,
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: _buttonBackgroundColor,
-                  foregroundColor: GlobalStyleVariables.primaryTextLightColour),
-              child: Text(buttonText)),
-        ],
+            ElevatedButton(
+                onPressed: isQuestReady
+                    ? startQuest
+                    : isQuestFinished
+                        ? claimQuest
+                        : noAction,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: _buttonBackgroundColor,
+                    foregroundColor:
+                        GlobalStyleVariables.primaryTextLightColour),
+                child: Text(buttonText)),
+            if (isQuestFinished)
+              const Text(
+                '* This will restart the app',
+                style: TextStyle(
+                    color: GlobalStyleVariables.questsButtonFinished,
+                    fontSize: 12),
+              ),
+            if (isQuestReady)
+              const Text(
+                '* This will restart the app',
+                style: TextStyle(
+                    color: GlobalStyleVariables.questsButtonReady,
+                    fontSize: 12),
+              ),
+          ],
+        ),
       ),
     );
   }
